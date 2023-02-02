cee5d0207d14a54697b77023b92b935c3db6df4c
Alfie <alfiegriffin100@gmail.com>
2023-02-02 01:04:57 +0000

    colour detection done

--- sideDetection.py
+++ sideDetection.py
@@ -1,51 +1,50 @@
 import cv2 as cv
 import numpy as np
+import time
 
-'''def find_closest_color(input_color):
-    color_list = [(255,255,255),(20,18,137),(172,72,13),(37,85,255),(76,155,25),(47,213,254)]
-    input_color = np.uint8([[input_color]])
-    input_hsv = cv.cvtColor(input_color, cv.COLOR_BGR2HSV)
-    closest_color = None
-    min_dist = float('inf')
-    for color in color_list:
-        color = np.uint8([[color]])
-        color_hsv = cv.cvtColor(color, cv.COLOR_BGR2HSV)
-        dist = np.sum((input_hsv - color_hsv) ** 2)
-        if dist < min_dist:
-            closest_color = color
-            min_dist = dist
-    return closest_color[0][0].tolist()'''
-        
+global max_val
+global min_val
 def find_closest_color(input_colour):
     colour_names =  ['white', 'red', 'blue', 'orange', 'green', 'yellow']
     colour_list = [(255,255,255),(20,18,137),(172,72,13),(37,85,255),(76,155,25),(47,213,254)]
-    print(input_colour)
-    h, s, v = cv.split(input_colour)
-    #white, ignore hue ideal (0,0,255)
-    if s < 10 and v > 100:
-        print(colour_names[0])
+    #print(input_colour)
+    l, a, b = cv.split(input_colour)
+    l = l[0][0]
+    a = a[0][0]
+    b = b[0][0]
+
+    for i in range(3):
+        c = input_colour[0][0][i]
+        if c > max_val[i]: 
+            max_val[i] = c
+        if c < min_val[i]: 
+            min_val[i] = c
+
+     # white
+    if l in range(84,184) and a in range(109,133) and b in range(135,155):
         return colour_list[0]
-    # red
-    if 0 < h and h < 60:
-        print(colour_names[1])
-        return colour_list[1]
-    # blue
-    if 240 < h and h < 300:
-        print(colour_names[2])
-        return colour_list[2]
-    #orange
-    if 0 < h and h < 120:
-        print(colour_names[3])
+    # orange
+    if l in range(76,133) and a in range(129,150) and b in range(163,182):
         return colour_list[3]
-    # green
-    if 120 < h and h < 180:
-        print(colour_names[4])
-        return colour_list[4]
     # yellow
-    if 60 < h and h < 120:
-        print(colour_names[5])
+    if l in range(101,175) and a in range(107,124) and b in range(170,195):
         return colour_list[5]
-    print('not known')
+    # green
+    if l in range(79,118) and a in range(95,108) and b in range(145,163):
+        return colour_list[4]    
+    # blue
+    if l in range(54,103) and a in range(115,131) and b in range(107,130):
+        return colour_list[2]
+    # red
+    if l in range(70,100) and a in range(132,150) and b in range(156,166):
+        return colour_list[1]
+    #print('not known',input_colour)
+
+
+def sort_by_location(c):
+    print(c)
+    x, y, w, h = cv.boundingRect(c)
+    return (y, x)
 
 # scale and show image for printing
 def showImg(label, img):
@@ -59,30 +58,19 @@ def showImg(label, img):
 
 def getColours(cube):
     
-    #scale image
-    scale = 0.5
-    width = int(cube.shape[1] *scale)
-    height = int(cube.shape[0] *scale)
-    dimensions = (width, height)
-    cube = cv.resize(cube, dimensions, interpolation=cv.INTER_AREA)
-
-
-    # set greyscale, blur and find edges
     grey = cv.cvtColor(cube, cv.COLOR_BGR2GRAY)
-    blur = cv.blur(cube,(5,5))
-    canny = cv.Canny(blur, 80, 120)
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
-    dilated = cv.dilate(canny, kernel)
+    blur = cv.blur(cube,(3,3))
+    canny = cv.Canny(blur, 55, 100, L2gradient = True) #60, 100
 
-    #showImg('edges',dilated)
+    showImg('edges',canny)
 
     # get contours
-    contours, hierarchies = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    contours, hierarchies = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
     # display contours
     blank = np.zeros(cube.shape, dtype='uint8')
     cv.drawContours(blank, contours, -1, (0,255,0), 1)
-    #showImg('contours',blank)
+    showImg('contours',blank)
 
     if hierarchies is None:
         return cube
@@ -91,6 +79,8 @@ def getColours(cube):
 
     output = cube
     colours = []
+    hsv_vals =[]
+    used_contours =[]
     # for each contour check if it is a square
     for i in range(len(contours)):
         contour = contours[i]
@@ -104,67 +94,67 @@ def getColours(cube):
         squareness = cv.norm(((perimeter / 4) * (perimeter / 4)) - area)
 
         # likely candidate for piece
-        if parent == -1 and area > 1000 and squareness < 150:
+        if parent == -1 and area > 2000 and area < 6000 and squareness < 200:#150
             
-            #print(i,area, squareness, parent)
+            print(area, squareness)
 
             # add contour to image
-            cv.drawContours(output, contours, i, (0,255,0), 1)
+            cv.drawContours(output, contours, i, (0,255,0), 2)
+            used_contours.append(contour)
 
             #get average colour
             mask = np.zeros(grey.shape, np.uint8) 
             cv.drawContours(mask, [contour], 0, 255, -1)
-            hsv_img = cv.cvtColor(np.uint8([[[255,255,255 ]]]), cv.COLOR_BGR2HSV)
-            #hsv_img = cv.cvtColor(cube, cv.COLOR_BGR2HSV)
-            #mean = cv.mean(hsv_img, mask=mask)
-            mean = np.uint8([[cv.mean(hsv_img)[:-1]]])
+            #mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+            lab_img = cv.cvtColor(cube, cv.COLOR_BGR2LAB)
+            mean = cv.mean(lab_img, mask=mask)[:-1]
+            hsv_vals.append([int(a) for a in mean])
+            mean = np.uint8([[mean]])
             colours.append(find_closest_color(mean))
-
-            '''  mask = np.zeros(grey.shape, np.uint8) 
-            cv.drawContours(mask, [contour], 0, 255, -1)
-
-            # get pixels inside contour
-            pixels = []
-            N = 0
-            for i in range(cube.shape[0]):
-                for j in range(cube.shape[1]):
-                    if mask[i][j] > 0 and (N % 5) == 0:
-                        pixels.append(cube[i][j])
-                        N+=1
-            pixels = np.uint8([pixels])
-            colours.append(avg_pixel(pixels))'''
-
-    print(colours)
+    
     if len(colours) == 4:
+        # order colours by contour location
+        #[colours for _, colours in sorted(zip(used_contours, colours))]
+        #print(hsv_vals)
+        #print('max', max_val)
+        #print('min', min_val)
         #create box in corner for colours
         side_len = 50
-        pos = [[0,1],[2,3]]
+        pos = [[0,2],[1,3]]
         for i in range(len(pos)):
             for j in range(len(pos[i])):
                 cv.rectangle(output, (i*side_len,j*side_len),((i+1)*side_len,(j+1)*side_len), colours[pos[i][j]],-1)
 
-
+        return True, output
     
-    return output
+    return False, output
 
-video = False
+max_val = [0,0,0]
+min_val = [255,255,255]
+
+video = True
 
 if video:
-    cap = cv.VideoCapture(2)
+    frame_rate = 20 # set frame rate
+    prev = 0
+    cap = cv.VideoCapture(0)
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
     while True:
-        # Capture frame-by-frame
+        time_elapsed = time.time() - prev
         ret, frame = cap.read()
-        # if frame is read correctly ret is True
+
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-        # Our operations on the frame come here
-        output = getColours(frame)
-        # Display the resulting frame
-        cv.imshow('frame', output)
+
+        if time_elapsed > 1./frame_rate:
+            prev = time.time()
+            face,output = getColours(frame)
+            # Display the resulting frame
+            showImg('frame',output)
+
         if cv.waitKey(1) == ord('q'):
             break
     # When everything done, release the capture
@@ -174,8 +164,8 @@ if video:
     cv.waitKey(0)
 
 else: #photo only
-    cube = cv.imread('C:\\Users\\Alfie\\Documents\\uni_work\\year3\\cs310\\github\Rubiks_solver\\good521.JPG')
-    #cube = cv.imread('/home/grifaj/Documents/y3project/Rubiks_solver/test1.jpg')
+    #cube = cv.imread('C:\\Users\\Alfie\\Documents\\uni_work\\year3\\cs310\\github\Rubiks_solver\\good521.JPG')
+    cube = cv.imread('/home/grifaj/Documents/y3project/Rubiks_solver/test1.jpg')
 
     output = getColours(cube)
     showImg('output', output)
